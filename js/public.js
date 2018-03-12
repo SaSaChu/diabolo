ecc38372501deb109e7e91f48c1bd3e294eaa310
SaSaChu <teresa7397@gmail.com>
2018-03-12 15:07:49 +0800

    index ok 剩下fb的手機版調整

--- js/public.js
+++ js/public.js
@@ -1,10 +1,27 @@
 /**
- * @author      OA Wu <comdan66@gmail.com>
+ * @author      ZEUS Design <service@zeusdesign.com.tw>
  * @copyright   Copyright (c) 2015 - 2018, OAF2E
  * @license     http://opensource.org/licenses/MIT  MIT License
- * @link        https://www.ioa.tw/
+ * @link        https://www.zeusdesign.com.tw
  */
  
 $(function () {
+
+  // 漢堡變換 + menu滑出
+  $(document).ready(function() {
+    $('#nav-icon3').click(function(){
+      $(this).toggleClass('open');
+
+      $('#r_menu_boxs').toggleClass('s_menu');
+      $('#menu_back').toggleClass('s_menu_b');
+    });
+  });
+
+  $('#r_menu_boxs').each (function () {
+    var $that = $(this);
+    $that.find ('span').click (function () {
+      $(this).toggleClass ('s');
+    });
+  });
 
 });
